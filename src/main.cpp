faa8a978619f5a22d08d0fb6805424ee7a70e121
Danh0DIY <dnn345169@gmail.com>
2025-08-13 22:47:57 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,23 @@
+#include <Arduino.h>
 #include <TFT_eSPI.h>
-#include "image.h" // ảnh RGB565
+#include "image.h" // ảnh RGB565 của bạn
 
 TFT_eSPI tft = TFT_eSPI();
 
 void setup() {
+    Serial.begin(115200);
+    Serial.println("Bắt đầu khởi tạo TFT_eSPI...");
+
     tft.init();
-    tft.setRotation(1);
+    tft.setRotation(1); // xoay ngang
+    tft.fillScreen(TFT_BLACK);
+
+    // Hiển thị ảnh từ file .h
     tft.pushImage(0, 0, IMAGE_WIDTH, IMAGE_HEIGHT, myImage);
+
+    Serial.println("Đã hiển thị ảnh!");
 }
 
-void loop() {}
+void loop() {
+    // Không làm gì
+}
