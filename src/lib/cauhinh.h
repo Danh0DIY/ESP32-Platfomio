27d26a5b266ccd8a94b4bcc17159ba2928d9f2b9
Danh0DIY <dnn345169@gmail.com>
2025-08-13 23:48:49 +0700

    Update cauhinh.h

--- src/lib/cauhinh.h
+++ src/lib/cauhinh.h
@@ -1,4 +1,3 @@
-//ESP32 C3
 #define ST7735_DRIVER
 #define ST7735_GREENTAB160x80  // chuẩn cho màn hình 160x80 ST7735S
 
@@ -16,6 +15,3 @@
 #define TFT_INVERSION_ON          // hoặc _OFF nếu bị âm
 
 #define SPI_FREQUENCY  27000000
-``
-
-Note: Cần chon Huge App trong Tools/Partition Scheme trên Arduino IDE để có được dung lượng 3MB
