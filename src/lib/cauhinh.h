fdacf7289d88e748fccb9a0683933d2afc27aaf8
Danh0DIY <dnn345169@gmail.com>
2025-08-13 22:30:40 +0700

    Create cauhinh.h

---
+++ src/lib/cauhinh.h
@@ -0,0 +1,19 @@
+#define ST7735_DRIVER
+#define TFT_WIDTH  160
+#define TFT_HEIGHT 80
+
+#define ST7735_GREENTAB160x80
+
+// Chân ESP32
+#define TFT_MOSI  5
+#define TFT_SCLK  18
+#define TFT_CS    19
+#define TFT_DC    21
+#define TFT_RST   22
+
+#define LOAD_GLCD
+#define LOAD_FONT2
+#define LOAD_FONT4
+#define SMOOTH_FONT
+
+#define SPI_FREQUENCY  27000000
