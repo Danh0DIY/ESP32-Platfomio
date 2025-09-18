f259cbd8a4b1e52d431bf72f4554e5733010b140
Danh0DIY <dnn345169@gmail.com>
2025-09-18 13:40:33 +0700

    Update cauhinh.h

--- src/lib/cauhinh.h
+++ src/lib/cauhinh.h
@@ -1,16 +1,20 @@
-// Chọn driver
 #define ST7789_DRIVER
 
-// Độ phân giải màn hình
 #define TFT_WIDTH  240
 #define TFT_HEIGHT 240
 
-// Chân SPI cho ESP32 DevKit V1
 #define TFT_MOSI 18
 #define TFT_SCLK 5
-#define TFT_CS   23   // Chip select
-#define TFT_DC   22   // Data/Command
-#define TFT_RST  19   // Reset
+#define TFT_CS   23
+#define TFT_DC   22
+#define TFT_RST  19
 
 #define LOAD_GLCD
 #define LOAD_FONT2
+#define LOAD_FONT4
+#define LOAD_FONT6
+#define LOAD_FONT7
+#define LOAD_FONT8
+#define LOAD_GFXFF
+
+#define SPI_FREQUENCY  40000000
