7f2e1a35bea59f693839fa3d8130e329430b3040
Danh0DIY <dnn345169@gmail.com>
2025-09-18 13:38:52 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,50 @@
-#include <TFT_eSPI.h> 
+#include <TFT_eSPI.h>
 #include <SPI.h>
 
-TFT_eSPI tft = TFT_eSPI();  // Khởi tạo TFT
+TFT_eSPI tft = TFT_eSPI();
+
+int pinX = 34;   // Joystick X
+int pinY = 35;   // Joystick Y
+int pinSW = 32;  // Joystick SW
+
+int cursorX = 120; 
+int cursorY = 120;
+int lastX = cursorX;
+int lastY = cursorY;
 
 void setup() {
   Serial.begin(115200);
-  tft.init();
-  tft.setRotation(0);  // Xoay màn hình 0-3
+  pinMode(pinSW, INPUT_PULLUP);
 
-  // Xóa màn hình
+  tft.init();
+  tft.setRotation(0);
   tft.fillScreen(TFT_BLACK);
 
-  // In chữ
-  tft.setTextColor(TFT_GREEN, TFT_BLACK);
-  tft.setTextSize(2);
-
-  // Vẽ hình chữ nhật
-  tft.fillRect(1,1,238,78, TFT_BLUE);
-  tft.fillRect(1,82,238,78, TFT_BLUE);
-  tft.fillRect(1,162,238,78, TFT_BLUE);
-  // Vẽ hình tròn
-  tft.fillCircle(120, 120, 40, TFT_BLUE);
-  
-  //chữ
-  
-  
-  tft.drawCentreString("Gai lon hong",120,42,2);
-  tft.drawCentreString("Dit gai xinh",120,122,2);
-  tft.drawCentreString("Dit gai nga",120,202,2);
-  
+  // Vẽ con trỏ ban đầu
+  tft.fillCircle(cursorX, cursorY, 3, TFT_GREEN);
 }
 
 void loop() {
+  int xValue = analogRead(pinX); 
+  int yValue = analogRead(pinY); 
+  int swValue = digitalRead(pinSW);
 
+  // Map ADC -> tọa độ
+  cursorX = map(xValue, 0, 4095, 0, 239);
+  cursorY = map(yValue, 0, 4095, 0, 239);
+
+  // Xóa con trỏ cũ
+  tft.fillCircle(lastX, lastY, 3, TFT_BLACK);
+
+  // Vẽ con trỏ mới
+  if (swValue == LOW) {
+    tft.fillCircle(cursorX, cursorY, 3, TFT_RED);   // Nhấn = đỏ
+  } else {
+    tft.fillCircle(cursorX, cursorY, 3, TFT_GREEN); // Không nhấn = xanh
+  }
+
+  lastX = cursorX;
+  lastY = cursorY;
+
+  delay(30);
 }
