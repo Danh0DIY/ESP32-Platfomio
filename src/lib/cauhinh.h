c4859fde959b361abc21edc0851212c586025f4d
Danh0DIY <dnn345169@gmail.com>
2025-09-20 14:52:32 +0700

    Update cauhinh.h

--- src/lib/cauhinh.h
+++ src/lib/cauhinh.h
@@ -3,11 +3,12 @@
 #define TFT_WIDTH  240
 #define TFT_HEIGHT 240
 
-#define TFT_MOSI 18
-#define TFT_SCLK 5
-#define TFT_CS   23
-#define TFT_DC   22
-#define TFT_RST  19
+#define TFT_MOSI 13
+#define TFT_SCLK 14
+#define TFT_CS   21
+#define TFT_DC   19
+#define TFT_RST  18
+#define TFT_BL   26
 
 #define LOAD_GLCD
 #define LOAD_FONT2
