e8b70d6fd10d9ebbfa0281b9e7353c3f6a625b56
Danh0DIY <dnn345169@gmail.com>
2025-09-23 21:55:55 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
-#define MIC_PIN 34
+// MAX9814 Test với ESP32
+const int micPin = 34;  // Chân OUT của MAX9814
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200);          // Bắt đầu Serial Monitor
+  analogReadResolution(12);      // ESP32 ADC 12-bit (0-4095)
+  analogSetAttenuation(ADC_11db); // Mở rộng dải đo (0-3.3V)
 }
 
 void loop() {
-  int micValue = analogRead(MIC_PIN);  // đọc ADC 0-4095
-  Serial.println(micValue);
-  delay(50);
+  int micValue = analogRead(micPin);  // Đọc giá trị analog
+  Serial.println(micValue);           // In ra Serial Monitor
+  delay(50);                          // 20 lần/giây
 }
