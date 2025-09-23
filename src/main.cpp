bdb5e044a0af706ab8d9430662615e939339b744
Danh0DIY <dnn345169@gmail.com>
2025-09-23 22:08:34 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,42 @@
 #include <Arduino.h>
-// MAX9814 Test với ESP32
-const int micPin = 34;  // Chân OUT của MAX9814
+#include <TFT_eSPI.h>  // Thư viện màn hình TFT
+
+// Cấu hình chân
+const int micPin = 34;  // MAX9814 OUT
+TFT_eSPI tft = TFT_eSPI(); // Khởi tạo TFT
+
+// Biến vẽ sóng
+int prevY = 0;
+int tftWidth, tftHeight;
 
 void setup() {
-  Serial.begin(115200);          // Bắt đầu Serial Monitor
-  analogReadResolution(12);      // ESP32 ADC 12-bit (0-4095)
-  analogSetAttenuation(ADC_11db); // Mở rộng dải đo (0-3.3V)
+  Serial.begin(115200);
+  analogReadResolution(12);
+  analogSetAttenuation(ADC_11db);
+
+  // Khởi tạo TFT
+  tft.init();
+  tft.setRotation(1); // Ngang
+  tft.fillScreen(TFT_BLACK);
+
+  tftWidth = tft.width();
+  tftHeight = tft.height();
+  prevY = tftHeight / 2;
 }
 
 void loop() {
-  int micValue = analogRead(micPin);  // Đọc giá trị analog
-  Serial.println(micValue);           // In ra Serial Monitor
-  delay(50);                          // 20 lần/giây
+  int micValue = analogRead(micPin); // Đọc tín hiệu mic (0-4095)
+  Serial.println(micValue);
+
+  // Chuyển giá trị ADC sang pixel TFT (dọc)
+  int y = map(micValue, 0, 4095, tftHeight, 0);
+
+  // Vẽ đường nối sóng âm
+  tft.drawLine(tftWidth - 2, prevY, tftWidth - 1, y, TFT_GREEN);
+  prevY = y;
+
+  // Dịch màn hình sang trái
+  tft.scroll(-1, 0);
+
+  delay(5); // Tốc độ vẽ
 }
