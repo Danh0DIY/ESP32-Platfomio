82dc5210f0d920105fb74c4312d96176dff00d58
Danh0DIY <dnn345169@gmail.com>
2025-09-24 16:51:16 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,49 @@
 #include <Arduino.h>
-#include <TFT_eSPI.h>
 #include "BluetoothA2DPSource.h"
 
-#define MIC_PIN 34
-
-TFT_eSPI tft = TFT_eSPI();
 BluetoothA2DPSource a2dp_source;
 
-int tftWidth, tftHeight;
-int prevY = 0;
-int x = 0;
+const int micPin = 34;   // Mic analog (MAX9814 OUT)
+int16_t sample_buffer[256];
 
-// Callback cho A2DP
-int32_t get_audio_data(Frame* frame, int32_t frame_count) {
-  for (int i = 0; i < frame_count; i++) {
-    int adcValue = analogRead(MIC_PIN) - 2048;  
-    int16_t sample = adcValue * 16;  // khuếch đại
-
-    frame[i].channel1 = sample;
-    frame[i].channel2 = sample;
+// Hàm lấy dữ liệu từ mic để gửi sang loa
+int32_t get_audio_data(Frame* data, int32_t frameCount) {
+  for (int i = 0; i < frameCount; i++) {
+    int micValue = analogRead(micPin);              // đọc ADC 0–4095
+    int16_t sample = map(micValue, 0, 4095, -32768, 32767);
+    data[i].channel1 = sample;   // kênh trái
+    data[i].channel2 = sample;   // kênh phải
   }
-  return frame_count;
+  return frameCount;
 }
 
 void setup() {
+  Serial.begin(115200);
   analogReadResolution(12);
-  analogSetAttenuation(ADC_11db);
 
-  // TFT
-  tft.init();
-  tft.setRotation(1);
-  tft.fillScreen(TFT_BLACK);
-  tftWidth = tft.width();
-  tftHeight = tft.height();
-  prevY = tftHeight / 2;
+  Serial.println("Đang scan loa Bluetooth...");
 
-  // Bluetooth
-  a2dp_source.start("TênLoaBluetooth", get_audio_data);
+  // Tìm thiết bị Bluetooth khả dụng
+  std::vector<BluetoothA2DPSource::BluetoothDevice> devices = a2dp_source.discover();
+
+  if (devices.size() == 0) {
+    Serial.println("Không tìm thấy loa nào!");
+    return;
+  }
+
+  // In ra danh sách để biết MAC
+  for (int i = 0; i < devices.size(); i++) {
+    Serial.printf("[%d] %s - %s\n", i, devices[i].name.c_str(), devices[i].address.toString().c_str());
+  }
+
+  // Ở đây chọn thiết bị đầu tiên
+  auto target = devices[0];
+  Serial.printf("Kết nối tới: %s (%s)\n", target.name.c_str(), target.address.toString().c_str());
+
+  // Bắt đầu stream mic → loa
+  a2dp_source.start(target.name.c_str(), get_audio_data, target.address.toString().c_str());
 }
 
 void loop() {
-  // Hiển thị sóng đơn giản
-  int micValue = analogRead(MIC_PIN);
-  int centered = micValue - 2048;
-  int y = (tftHeight / 2) - centered / 20;
-
-  if (y < 0) y = 0;
-  if (y > tftHeight - 1) y = tftHeight - 1;
-
-  tft.drawLine(x, 0, x, tftHeight, TFT_BLACK);
-  tft.drawLine(x, prevY, x, y, TFT_GREEN);
-  prevY = y;
-
-  x++;
-  if (x >= tftWidth) x = 0;
-
-  delay(5);
+  // Không cần thêm code ở loop
 }
