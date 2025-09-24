c6111a7cd506e234492e4abed58a809d50ae685b
Danh0DIY <dnn345169@gmail.com>
2025-09-24 16:42:33 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,26 +11,23 @@ int tftWidth, tftHeight;
 int prevY = 0;
 int x = 0;
 
-// Callback cho A2DP (phát ra loa Bluetooth)
+// Callback cho A2DP
 int32_t get_audio_data(Frame* frame, int32_t frame_count) {
   for (int i = 0; i < frame_count; i++) {
-    int adcValue = analogRead(MIC_PIN) - 2048;  // bỏ offset
-    int16_t sample = adcValue * 16;             // khuếch đại -> 16-bit
+    int adcValue = analogRead(MIC_PIN) - 2048;  
+    int16_t sample = adcValue * 16;  // khuếch đại
 
-    frame[i].channel1 = sample;  // kênh trái
-    frame[i].channel2 = sample;  // kênh phải
+    frame[i].channel1 = sample;
+    frame[i].channel2 = sample;
   }
   return frame_count;
 }
 
 void setup() {
-  Serial.begin(115200);
-
-  // Mic
   analogReadResolution(12);
   analogSetAttenuation(ADC_11db);
 
-  // Màn hình
+  // TFT
   tft.init();
   tft.setRotation(1);
   tft.fillScreen(TFT_BLACK);
@@ -38,23 +35,20 @@ void setup() {
   tftHeight = tft.height();
   prevY = tftHeight / 2;
 
-  // Bluetooth A2DP Source
-  a2dp_source.start("Tên_loa_Bluetooth", get_audio_data);  
+  // Bluetooth
+  a2dp_source.start("TênLoaBluetooth", get_audio_data);
 }
 
 void loop() {
-  // Đọc giá trị mic để hiển thị sóng
+  // Hiển thị sóng đơn giản
   int micValue = analogRead(MIC_PIN);
-  int centered = micValue - 2048;       // lấy dao động quanh mức giữa
+  int centered = micValue - 2048;
   int y = (tftHeight / 2) - centered / 20;
 
   if (y < 0) y = 0;
   if (y > tftHeight - 1) y = tftHeight - 1;
 
-  // Xóa cột trước
   tft.drawLine(x, 0, x, tftHeight, TFT_BLACK);
-
-  // Vẽ sóng âm
   tft.drawLine(x, prevY, x, y, TFT_GREEN);
   prevY = y;
 
