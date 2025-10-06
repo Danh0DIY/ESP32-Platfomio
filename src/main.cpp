5fab5e7b83a82cfa658e262600ec240cb018fab2
Danh0DIY <dnn345169@gmail.com>
2025-10-06 20:57:03 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,11 @@ void setup() {
   tft.init();
   tft.setRotation(0);
   tft.fillScreen(TFT_BLACK);
+  for (int x=0, x <=240, x +=60) {
+  	for (int y=0, y<=240, y +=60) {
+  		tft.fillRect(x, y, TFT_WHITE);
+  		}
+  	}
 }
 
 void loop() {
